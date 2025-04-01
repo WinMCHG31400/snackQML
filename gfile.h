d26cc61c17574d4191007beccb125a42ecf623c8
winmchg31400 <WinMCHG31400@Outlook.com>
2025-04-01 11:29:59 +0800

    v11.2

--- gfile.h
+++ gfile.h
@@ -3,7 +3,6 @@
 
 #include <QObject>
 #include <QDir>
-#include "Base.h"
 
 class GFile : public QObject
 {
@@ -21,24 +20,8 @@ public:
     Q_INVOKABLE QString source() { return m_source; }
     QString m_source;
 
-    void restart(){
-        c20=new Base;
-    }
-    int getgold(){
-        return c20->getgold();
-    }
-    bool move(int type){
-        return c20->move(type);
-    }
-    int show(int x, int y){
-        return c20->show(x,y);
-    }
-
-
 signals:
     void sourceChanged(const QString& source);
-private:
-    Base* c20;
 
 
 
--- snack.pro
+++ snack.pro
@@ -13,7 +13,6 @@ RC_FILE = icon.rc
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        base.cpp \
         gfile.cpp \
         main.cpp
 
@@ -31,5 +30,4 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
-    Base.h \
     gfile.h
